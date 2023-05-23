e2b953f6ba544c4e38b432d3df4ab57a0c0751e2
Mattia <s1093418@studenti.univpm.it>
2023-05-23 19:17:02 +0200

    login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,14 +13,14 @@ class DatabaseSeeder extends Seeder {
 
     public function run() {
 
-       /* DB::table('companies')->insert([
+       DB::table('companies')->insert([
             ['id' => 1, 'P_Iva' => '12345678910', 'Ragione_Sociale' => 'Coal', 'Localizzazione' => 'Daje Roma Dajeeee 10', 'Descrizione' => 'Lorem Ipsum', 'Logo' => 'lebron.jpg'],
             ['id' => 2, 'P_Iva' => '11121314151', 'Ragione_Sociale' => 'Crai', 'Localizzazione' => 'Evviva Gesu 33', 'Descrizione' => 'Lorem Ipsum', 'Logo' => 'lebron.jpg'],
             ['id' => 3, 'P_Iva' => '61718192021', 'Ragione_Sociale' => 'Oasi', 'Localizzazione' => 'Piazza Roma 5', 'Descrizione' => 'Lorem Ipsum', 'Logo' => 'lebron.jpg'],
             ['id' => 4, 'P_Iva' => '22232425262', 'Ragione_Sociale' => 'Simply', 'Localizzazione' => 'Via Gianfranco 3', 'Descrizione' => 'Lorem Ipsum', 'Logo' => 'lebron.jpg'],
             ['id' => 5, 'P_Iva' => '72829303132', 'Ragione_Sociale' => 'Lidl', 'Localizzazione' => 'Via Gigio 14', 'Descrizione' => 'Lorem Ipsum', 'Logo' => 'lebron.jpg'],
             ['id' => 6, 'P_Iva' => '33343536373', 'Ragione_Sociale' => 'Eurospin', 'Localizzazione' => 'Via Franco 12', 'Descrizione' => 'Lorem Ipsum', 'Logo' => 'lebron.jpg'],
-        ]);*/
+        ]);
 
         DB::table('registers')->insert([
             ['ID_Registrato' => 1, 'Utente' => 'useruser', 'Password' => Hash::make('Yg4Xzxjb'), 'Nome' => 'Franco', 'Cognome' => 'Rossi', 'Genere' => 'Uomo', 'Tipo' => 'user', 'Email' => 'franco@rossi.it', 'Telefono' => '3333102759', 'Data_Nascita' => '1945/10/05'],
@@ -28,7 +28,7 @@ class DatabaseSeeder extends Seeder {
             ['ID_Registrato' => 3, 'Utente' => 'adminadmin', 'Password' => Hash::make('Yg4Xzxjb'), 'Nome' => 'LeBron', 'Cognome' => 'James', 'Genere' => 'Uomo', 'Tipo' => 'admin', 'Email' => 'lebron@james.it', 'Telefono' => '3493102759', 'Data_Nascita' => '2001/11/22'],
         ]);
 
-       /* DB::table('offers')->insert([
+       DB::table('offers')->insert([
             ['ID_Offerta' => 1, 'Descrizione' => self::DESCPROD, 'Scadenza' => '1945/10/05', 'Immagine' => 'lebron.jpg', 'ID_Azienda' => 2],
             ['ID_Offerta' => 2, 'Descrizione' => self::DESCPROD, 'Scadenza' => '2024/10/05', 'Immagine' => 'lebron.jpg', 'ID_Azienda' => 1],
             ['ID_Offerta' => 3, 'Descrizione' => self::DESCPROD, 'Scadenza' => '2047/10/05', 'Immagine' => 'lebron.jpg', 'ID_Azienda' => 4],
@@ -36,7 +36,6 @@ class DatabaseSeeder extends Seeder {
             ['ID_Offerta' => 5, 'Descrizione' => self::DESCPROD, 'Scadenza' => '1492/10/12', 'Immagine' => 'lebron.jpg', 'ID_Azienda' => 5],
             ['ID_Offerta' => 6, 'Descrizione' => self::DESCPROD, 'Scadenza' => '1789/07/14', 'Immagine' => 'lebron.jpg', 'ID_Azienda' => 6],
         ]);
-        */
     }
 
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,30 +6,23 @@
 <section>  
     <div class="row justify-content-center">
         <div class="col-12 col-lg-5 text-center">
-            <h2 class="fw-semibold mb-4">Effettua l'accesso</h2>            
+            <h2 class="fw-semibold mb-4">Effettua l'accesso</h2> 
+            {{ Form::open(array('route' => 'login', 'class' => 'contact-form')) }}           
                 @csrf
                 <div class="form-floating mb-3">
-                    
-                    <input name="Utente" type="text" class="form-control border-top-0 border-start-0 border-end-0 border-2 border-black  border-bottom-3 rounded-0 bg-transparent no-outline" placeholder="Username" required="true">
-                    <label>Username</label>
+                    {{ Form::label('Utente', 'Nome Utente', ['class' => 'label-input']) }}
+                    {{ Form::text('Utente', '', ['class' => 'input form-control border-top-0 border-start-0 border-end-0 border-2 border-black  border-bottom-3 
+                    rounded-0 bg-transparent no-outline','id' => 'Utente']) }}                    
                 </div>
-                <div class="form-floating mb-3">
-                    
-                    <input name="Password" type="password" class="form-control border-top-0 border-start-0 border-end-0 border-2 border-black  border-bottom-3 rounded-0 bg-transparent no-outline" placeholder="Password" required="true">
-                    <label>Password</label>
+                <div class="form-floating mb-3">      
+                    {{ Form::label('password', 'Password', ['class' => 'label-input']) }}
+                    {{ Form::password('Password', ['class' => 'input form-control border-top-0 border-start-0 border-end-0 border-2 border-black  border-bottom-3 
+                        rounded-0 bg-transparent no-outline', 'id' => 'Password']) }}
                 </div>
-                
-                <button type="submit" class="loader border-0 bg-black text-white p-3 text-center fw-bold text-uppercase d-block w-100 mb-3 lh-1 rounded">accedi</button>
-                <a href="{{ route('registrazione') }}" class="text-black d-block spa">Se non sei ancora registrato crea il tuo account</a>
-            </form>
-            {{ Form::open(array('route' => 'login', 'class' => 'contact-form')) }}
-                {{ Form::label('Utente', 'Nome Utente', ['class' => 'label-input']) }}
-                {{ Form::text('Utente', '', ['class' => 'input','id' => 'Utente']) }}
-                {{ Form::label('password', 'Password', ['class' => 'label-input']) }}
-                {{ Form::password('Password', ['class' => 'input', 'id' => 'Password']) }}
                 {{ Form::submit('Login', ['class' => 'loader border-0 bg-black text-white p-3 text-center fw-bold text-uppercase d-block w-100 mb-3 lh-1 rounded']) }}
+                <a href="{{ route('registrazione') }}" class="text-black d-block spa">Se non sei ancora registrato crea il tuo account</a>       
             {{ Form::close() }}
-        </div>
+        </div>   
     </div>
 </section>
 @endsection
