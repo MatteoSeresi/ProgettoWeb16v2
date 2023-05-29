eb4244d9037f7b3fcef52dc8f91ccc8887b48bdd
Diego7imi <diegosettimi01@gmail.com>
2023-05-29 17:05:09 +0200

    home funzionante

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,40 +3,40 @@
 @section('title', 'Home')
 
 @section('content')
-@isset($aziende)
-        
-            <section>
-                <div class="slideshow-container">
-                @foreach ($aziende as $azienda)
-                    <div class="mySlides fade">
-                    <img src="../../public/images/aziende/{{ $azienda->Logo }}">
-                    </div>
-                @endforeach
-                </div>
-                <br>
-                <div style="text-align:center">
-                    <span class="dot" onclick="currentSlide(1)"></span> 
-                    <span class="dot" onclick="currentSlide(2)"></span> 
-                    <span class="dot" onclick="currentSlide(3)"></span> 
-                </div>
-                <br>
-                <br>
+<section>
+    <div class="slideshow-container">
+    @isset($aziende)
+      @foreach ($aziende as $azienda)
+          <div class="mySlides fade">
+          <img src="../public/images/aziende/{{ $azienda->Logo }}">
+          </div>
+      @endforeach
+    @endisset
+    </div>
+    <br>
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span> 
+        <span class="dot" onclick="currentSlide(2)"></span> 
+        <span class="dot" onclick="currentSlide(3)"></span> 
+    </div>
+    <br>
+    <br>
 
-                
-                <div class="row">
-                <h2>Descrizione</h2>
-                    <div class="col">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut dui dapibus, commodo nunc sed, molestie turpis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi. Nullam vel consectetur neque. Nulla facilisi. Sed viverra est a urna aliquam bibendum</p> 
-                       
-                    </div>
-                    <div class="col">
-                        <p> Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc hendrerit, felis sed tincidunt volutpat, odio lorem viverra lectus, sed suscipit justo libero sit amet urna. Aliquam non elit eu justo rutrum euismod eu in lacus. Donec interdum diam in purus mollis, id convallis dui fringilla. In hac habitasse platea dictumst. Nullam nec nisi commodo, facilisis diam id, condimentum dolor. </p>
-                      
-                    </div>
-                </div>
+    
+    <div class="row">
+    <h2>Descrizione</h2>
+        <div class="col">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut dui dapibus, commodo nunc sed, molestie turpis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi. Nullam vel consectetur neque. Nulla facilisi. Sed viverra est a urna aliquam bibendum</p> 
+            
+        </div>
+        <div class="col">
+            <p> Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc hendrerit, felis sed tincidunt volutpat, odio lorem viverra lectus, sed suscipit justo libero sit amet urna. Aliquam non elit eu justo rutrum euismod eu in lacus. Donec interdum diam in purus mollis, id convallis dui fringilla. In hac habitasse platea dictumst. Nullam nec nisi commodo, facilisis diam id, condimentum dolor. </p>
+          
+        </div>
+    </div>
 
-            </section>
-            <script>
+</section>
+<script>
   let slideIndex = 1;
   showSlides(slideIndex);
 
@@ -69,5 +69,4 @@
     plusSlides(1); // Cambia 1 con il numero di slide da mostrare automaticamente
   }, 2000); // Imposta l'intervallo di tempo in millisecondi tra ogni slide (in questo esempio 2000ms = 2 secondi)
 </script>
-@endisset
 @endsection
