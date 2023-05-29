da0e4bfdebdc9478ea8560bb18a9d78e5582d581
Diego7imi <diegosettimi01@gmail.com>
2023-05-29 11:10:44 +0200

    filtri funzionanti

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 <div id="filter">
-    <input id="desc" type="text" placeholder="Descrizione">
+    <!--<input id="desc" type="text" placeholder="Descrizione">
     <div class="dropdown">
         <button onclick="myFunction()" class="dropbtn" id="bot">Aziende<i class="fa fa-angle-down" aria-hidden="true"></i></button>
         <div id="myDropdown" class="dropdown-content">
@@ -14,72 +14,90 @@
             <a href="#about">About</a>
         </div>
     </div>
-    <input id="invio" type="submit" value="Filtra">
+    <input id="invio" type="submit" value="Filtra">-->
+    <input type="text" id="filter-azienda" placeholder="Filtro per azienda">
+    <input type="text" id="filter-descrizione" placeholder="Filtro per descrizione">
 </div>
 
-@isset($aziende)
-    @foreach ($aziende as $azienda)
-        @isset($azienda->offerte)
-            @if(count($azienda->offerte) > 0)
-                <div id="catalogo">
-                    <div id="name_a">
-                        <h1>{{ $azienda->Ragione_Sociale}}</h1>
-                    </div>
-                    @foreach ($azienda->offerte as $offer)
-                        <div id="promo">
-                            <div id="left_c">
-                                <h2>{{ $offer->Nome }}</h2>
-                                <p>Data di scadenza: {{ $offer->Scadenza }}</p>
-                                <p>Descrizione: {{ $offer->Descrizione }}</p>
-                                <a href="#">Genera Coupon</a>
-                            </div>
-                            <div id="right_c">
-                                <img src="../../public/images/{{ $offer->Immagine }}" alt="Coupon">
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-            @endif
-        @endisset
-    @endforeach
-@endisset
+<div id="aziende-container">
+  @isset($aziende)
+      @foreach ($aziende as $azienda)
+          @isset($azienda->offerte)
+              @if(count($azienda->offerte) > 0)
+                  <div class="catalogo azienda">
+                      <div class="name_a">
+                          <h1>{{ $azienda->Ragione_Sociale}}</h1>
+                      </div>
+                      @foreach ($azienda->offerte as $offer)
+                          <div class="promo">
+                              <div class="left_c">
+                                  <h2>{{ $offer->Nome }}</h2>
+                                  <p>Data di scadenza: {{ $offer->Scadenza }}</p>
+                                  <p>Descrizione: {{ $offer->Descrizione }}</p>
+                                  <a href="#">Genera Coupon</a>
+                              </div>
+                              <div class="right_c">
+                                  <img src="../../public/images/{{ $offer->Immagine }}" alt="Coupon">
+                              </div>
+                          </div>
+                      @endforeach
+                  </div>
+              @endif
+          @endisset
+      @endforeach
+  @endisset
+</div>
 @endsection
 
 
 
 <script>
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
 
 $(document).ready(function() {
-  // Funzione di filtro
-  function filterOffers() {
-    var searchDesc = $('#desc').val().toUpperCase();
-    var searchCompany = $('#myInput').val().toUpperCase();
-
-    $('.promo').each(function() {
-      var offerDesc = $(this).find('p:nth-child(3)').text().toUpperCase();
-      var companyName = $(this).closest('.catalogo').find('.name_a h1').text().toUpperCase();
-
-      if ((searchDesc === '' || offerDesc.includes(searchDesc)) &&
-          (searchCompany === '' || companyName.includes(searchCompany))) {
-        $(this).show();
-      } else {
-        $(this).hide();
-      }
+    // Evento di ascolto per il pulsante di filtro
+    $('#filter-button').on('click', function() {
+        filterResults();
     });
-  }
 
-  // Evento di ricerca per descrizione
-  $('#desc').keyup(function() {
-    filterOffers();
-  });
+    // Evento di ascolto per gli input di filtro
+    $('#filter-azienda, #filter-descrizione').on('input', function() {
+        filterResults();
+    });
 
-  // Evento di ricerca per azienda
-  $('#myInput').keyup(function() {
-    filterOffers();
-  });
+    function filterResults() {
+      var aziendaFilter = $('#filter-azienda').val().toLowerCase();
+      var descrizioneFilter = $('#filter-descrizione').val().toLowerCase();
+
+      $('.azienda').each(function() {
+          var aziendaName = $(this).find('.name_a h1').text().toLowerCase();
+          var offerte = $(this).find('.promo');
+          var showAzienda = false;
+
+          // Nascondi l'azienda inizialmente
+          $(this).hide();
+
+          offerte.each(function() {
+              var nomeOfferta = $(this).find('.left_c h2').text().toLowerCase();
+              var descrizioneOfferta = $(this).find('.left_c p:eq(1)').text().toLowerCase();
+
+              // Mostra l'offerta solo se corrisponde ai filtri
+              if (aziendaName.includes(aziendaFilter) && (nomeOfferta.includes(descrizioneFilter) || descrizioneOfferta.includes(descrizioneFilter))) {
+                  showAzienda = true;
+                  return false; // Esci dal ciclo each per le offerte
+              }
+          });
+
+          // Mostra o nascondi l'azienda in base alla corrispondenza dei filtri
+          if (showAzienda) {
+              $(this).show();
+          } else {
+              $(this).hide();
+          }
+      });
+    }
 });
 
 </script>
